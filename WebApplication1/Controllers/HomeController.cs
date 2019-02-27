4ee6ea5f74552b88aac3884022c9ba48d67499ce
Allann Hoffmann <allann.hof@gmail.com>
2019-02-27 15:45:54 -0300

    SO testes

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace WebApplication1.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["teste"] = "Só fazendo um teste.";
 
             return View();
         }
