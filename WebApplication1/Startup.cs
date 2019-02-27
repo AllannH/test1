03391d1bb61244933245c2a9e5a77d064d161223
Allann Hoffmann <allann.hof@gmail.com>
2019-02-27 17:52:07 -0300

    tema

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
 
 namespace WebApplication1
 {
@@ -33,6 +36,9 @@ namespace WebApplication1
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApplication1Context>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("WebApplication1Context"), builder => builder.MigrationsAssembly("WebApplication1")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
